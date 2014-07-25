89421fb208516a5b800c4970458251ade450d215
sachinhd <sachin28new@gmail.com>
2014-07-25 19:19:00 +0530

    added homepage

---
+++ homepage.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'inc/connection.php';
+include 'inc/head2nav.php';
+include 'inc/get_bloginfo.php';
+include 'inc/get_cat.php';?>
+<div id="matter">
+	<?php include 'inc/homepageband.php';
+	include 'inc/leftcolumn.php';
+	include 'inc/homepagecontent.php';
+	include 'inc/rightcolumn.php'; ?>
+</div>
+<?php
+include 'inc/js.php';
+include 'inc/footer.php';
+?>
